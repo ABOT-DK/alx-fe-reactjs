9d844fcc4ce78b2ed892120183ef0c22d53200bf
ABOT-DK <davidkabotsi@gmail.com>
2025-07-20 16:10:14 +0000

    added Counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
   return (
     <>
       <div>
+        <Counter />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <WelcomeMessage />
         <Header />
