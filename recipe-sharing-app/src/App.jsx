c6cba42ec57381bc4560335a0d767c8897c4ccf1
ABOT-DK <davidkabotsi@gmail.com>
2025-07-27 19:38:01 +0000

    add few functionalities

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import AddRecipeForm from './componenets/AddRecipeForm'
-import RecipeList from './componenets/RecipeList'
+import { Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import EditRecipeForm from './components/EditRecipeForm';
+import DeleteRecipeButton from './components/DeleteRecipeButton';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <RecipeList />
-        <AddRecipeForm />
-      </div>
-    </>
-  )
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
