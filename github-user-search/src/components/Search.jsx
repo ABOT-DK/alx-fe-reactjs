beef5c165ee9d0640b5eb34e473ce7e5d5d58413
ABOT-DK <davidkabotsi@gmail.com>
2025-08-03 09:33:02 +0000

    added advanced search functionalities

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { advancedUserSearch } from '../services/githubService';
+import { advancedUserSearch, fetchUserData } from '../services/githubService';
 
 function Search() {
   const [form, setForm] = useState({
