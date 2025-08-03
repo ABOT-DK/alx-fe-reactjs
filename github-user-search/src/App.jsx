152ca6c7b1bee5697261186d4a1404832447cfe2
ABOT-DK <davidkabotsi@gmail.com>
2025-08-03 08:50:34 +0000

    added functionalities to github-search

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div style={{ padding: '2rem' }}>
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
       <h1>GitHub User Search</h1>
-      <p>Start building your search interface here.</p>
+      <Search />
     </div>
   );
 }
